a847227813a4ee53291c04536facbbe15ac70024
Harley Espinoza <harley@baum.digital>
2017-10-07 20:52:56 -0600

    storage in dashboard

--- js/startmin.js
+++ js/startmin.js
@@ -11,15 +11,225 @@ Conexion
       };
      firebase.initializeApp(config)
 
+//------------ ROUTES ------------------
+/*PRODUCTS*/
+    const PRODUCTS = "storage/products";
+    const CATEGORY_PRODUCTS=  PRODUCTS+"/categories";
 
+/*ORDERS*/
+    const ORDERS = "storage/orders";
+    const ORDER_PROFUCTS=  ORDERS+"/categories";
+
+
+//---------- MANAGEMENT DATA METHODS -----------
+//ADD NODES
+const addData = (route, obj)=>{
+  firebase.database()
+          .ref(route)
+          .set({...obj})
+          .then(()=>console.log("Realizado con exito!"))
+          .catch((error)=>console.log("Error: "+error));
+}
+
+//DELETE
+const deleteData=(route)=>{
+    alert(route);
+}
+//DISPLAY   
+const loadDataTable=(route)=>{
+  let val,category,subcategoria,content,id;
+  firebase.database()
+          .ref(route)
+          .on('value', function(snapshot){
+              if(snapshot.exists()) snapshot.forEach(function(data){ 
+                  val= data.val();
+                  category = Object.keys(val)[0];
+                  category = Object.getOwnPropertyDescriptor(val, category);
+                  subcategory = Object.keys(category.value)[0];
+                  subcategory = Object.getOwnPropertyDescriptor(category.value, subcategory);
+                  product = subcategory.value;
+                  id =Object.keys(category.value)[0];
+                  content +='<tr class="gradeX" >';
+                  content += '<td class="center">' + id + '</td>';
+                  content += '<td class="center">' + product.name + '</td>';
+                  content += '<td class="center">' + product.description + '</td>';
+                  content += '<td>' + product.lot + '</td>';
+                  content += '<td>' + product.price + '</td>';
+                  content += '</tr>';
+                  
+                  });
+                //$('#dataTable').append(content); 
+          });
+                           
+                      //$('#dataTables-example').append(content);
+}                
+                      /* 
+
+                      */
+
+          
+         // $('#dataTables-example').append(content);
+
+    /*
+    
+   <tr class="gradeA">
+                                                <td>Misc</td>
+                                                <td>NetFront 3.1</td>
+                                                <td>Embedded devices</td>
+                                                <td class="center">-</td>
+                                                <td class="center">C</td>
+                                            </tr>
+
+    
+     <tr class="odd gradeX">
+                                                <td>Trident</td>
+                                                <td>Internet Explorer 4.0</td>
+                                                <td>Win 95+</td>
+                                                <td class="center">4</td>
+                                                <td class="center">X</td>
+                                            </tr>
+     */
+
+
+           /*let data=snapshot.val();
+
+      let category = Object.keys(data)[0];
+      category = Object.getOwnPropertyDescriptor(data, category);
+      data = category.value;
+      let subCategory = Object.keys(data)[0];
+      console.log(subCategory);
+      subCategory = Object.getOwnPropertyDescriptor(data,subCategory);
+      data = subCategory.value;
+      let id = Object.keys(data)[0]
+      id = Object.getOwnPropertyDescriptor(data,id);
+      let product = id.value;
+      console.log(product);
+      }
+   
+
+                      //console.log(Object.keys("subcategory: " +subCategory));
+                      //console.log(Object.entries(val))
+                      //
+                     /* new Array(rows)
+                    .fill(null)
+                    .map(
+                      (e, i) => {
+                        points=points-5;
+                        top=top-5
+                        return(
+                          <StampsView key={i} stamps={points} top={top<5?top:5} />
+                        )
+                      }
+                    )*/
+ 
+          /*var content = '';
+          content +='<tr class="gradeA" >';
+                      content += '<td class="center">' + "subcategory "+ '</td>';
+                      content += '<td class="center">' + "product.name" + '</td>';
+                      content += '<td class="center">' + "product.description" + '</td>';
+                      content += '<td>' + "product.lot" + '</td>';
+                      content += '<td>' + "product.price "+ '</td>';
+                    
+                      content += '</tr>';
+
+                      content +='<tr class="gradeX" >';
+                      content += '<td class="center">' + "subcategory "+ '</td>';
+                      content += '<td class="center">' + "product.name" + '</td>';
+                      content += '<td class="center">' + "product.description" + '</td>';
+                      content += '<td>' + "product.lot" + '</td>';
+                      content += '<td>' + "product.price "+ '</td>';
+                    
+                      content += '</tr>';*/
+
+
+//CONTROLLER METHODS
+
+/*
+    Clean inputs
+ */
+ function _reset(){
+       $("#txt_productID").val("");
+       $("#txt_barCode").val("");
+       $("#txt_productName").val("");
+       $("#txt_productModel").val("");
+       $("#txt_price").val("");
+       $("#txt_oldPrice").val("");
+       $("#txt_trademark").val("");
+       $("#txt_productSize").val("");
+       $("#ta_descripPro").val("");
+       location.reload(); 
+  }
+
+/*
+    Listening page actions
+ */function _onChangeStatusPage(catalog){
+    //VARS
+      let materials = {};
+      let category,subCategory,id,barCode,name,model,lot,price,oldPrice,tradeMark,size,description;
+    //KEYUP LISTENER   
+      $("#txt_productID").keyup(()=>{id=$("#txt_productID").val()});
+      $("#txt_barCode").keyup(()=>{ barCode=$("#txt_barCode").val()});
+      $("#txt_productName").keyup(()=>{ name=$("#txt_productName").val()});
+      $("#txt_productModel").keyup(()=>{ model=$("#txt_productModel").val()});
+      $("#txt_lot").keyup(()=>{ lot=$("#txt_lot").val()});
+      $("#txt_price").keyup(()=>{ price=$("#txt_price").val()});
+      $("#txt_oldPrice").keyup(()=>{ oldPrice=$("#txt_oldPrice").val()});
+      $("#txt_trademark").keyup(()=>{ tradeMark=$("#txt_trademark").val()});
+      $("#txt_productSize").keyup(()=>{ size=$("#txt_productSize").val()});
+      $("#ta_descripPro").keyup(()=>{ description=$("#ta_descripPro").val()});
+    //CHANGE LISTENER 
+      $('#dd_category').change(()=>{category=$('#dd_category').val()}); 
+      $('#dd_subCategory').change(()=>{subCategory=$('#dd_subCategory').val()}); 
+      $('#dd_materials').change(function () {
+      $("#dd_materials option:selected" ).each(function(i) {
+                                                              materials[i]=$( this ).text();
+                                                              });
+                                          })
+                                          .change();
+    //CLICKS LISTENER                                        
+      $('#item_table').click(()=>{showPage("tbl_products",true)});
+      $('#item_frm').click(()=>{showPage("frm_products",true)});
+      $('#btn_goForm').click(()=>{showPage("frm_products",true)});
+      $('#btn_reset').click(()=>{_reset()});
+      $('#btn_push').click(()=> {_addProduct(category,subCategory,id,barCode,name,model,lot,price,oldPrice,tradeMark,size,description,materials,catalog)});
+    //METHODS
+    loadDataTable(CATEGORY_PRODUCTS);
+    //DATATABLES
+     $('#dataTables-example').DataTable({responsive: true});
+  }   
+
+/*
+    Controller data Product to be added
+ */function _addProduct(category,subCategory,id,barCode,name,model,lot,price,oldPrice,tradeMark,size,description,materials,catalog){
+        const product={
+            category,
+            subCategory,
+            barCode,
+            name,
+            model,
+            lot,
+            price,
+            oldPrice,
+            tradeMark,
+            size,
+            description,
+            materials,
+            catalog
+        };
+       let route = CATEGORY_PRODUCTS+"/"+product.category+"/"+product.subCategory+"/"+id+"/";
+        console.log(route);
+        console.log(product);
+        addData(route,product)
+         
+    }
 // --------------- NAVEGATION DASHBOARD ---------------
 
 /*
   Animation to navigation page
- */const showPage=(id="tbl_producs",display="true")=>{//frm || tbl 
+ */const showPage=(id="tbl_products",display="true")=>{//frm || tbl 
       //hide all page here!
       $("#frm_products").css("display", "none");
-      $("#tbl_producs").css("display", "none");
+      $("#tbl_products").css("display", "none");
 
       let page =$("#"+id);
       page.css("display", display?"display":"none");
@@ -97,17 +307,12 @@ $(document).on('ready', function() {
           
         });
       
-    });
-
-  $(document).ready(function() {
-                $('#dataTables-example').DataTable({
-                        responsive: true
-                });
-            });
+    });             
+  
 
 //---------------------- DASHBOARD ADMINISTRATOR FORM LISTENER -------------------------------
     _onChangeStatusPage(catalog);
-
+    
 });
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
@@ -140,147 +345,12 @@ $(function() {
     }
 });
 
-//------------ ROUTES ------------------
-/*PRODUCTS*/
-    const PRODUCTS = "storage/products";
-    const CATEGORY_PRODUCTS=  PRODUCTS+"/categories";
-
-/*ORDERS*/
-    const ORDERS = "storage/orders";
-    const ORDER_PROFUCTS=  ORDERS+"/categories";
-
-//---------- MANAGEMENT DATA METHODS -----------
-//ADD NODES
-const addData = (route, obj)=>{
-  firebase.database()
-          .ref(route)
-          .set({...obj})
-          .then(()=>console.log("Realizado con exito!"))
-          .catch((error)=>console.log("Error: "+error));
-}
-
-//DELETE
-const deleteData=(route)=>{
-    alert(route);
-}
-//DISPLAY   
-const loadDataTable=(route)=>{
-  firebase.database()
-          .ref()
-          .once('value', function(snapshot){
-                  if(snapshot.exists()){
-                  var content = '';
-                  snapshot.forEach(function(data){
-                      var val = data.val();
-                      content +='<tr>';
-                      content += '<td>' + val.descripcion + '</td>';
-                      content += '<td>' + val.direccion + '</td>';
-                      content += '<td>' + val.estado + '</td>';
-                      content += '<td>' + val.imagen + '</td>';
-                      content += '<td>' + val.tipo + '</td>';
-                      content += '<td>' + val.udisplayName + '</td>';
-                      content += '<td>' + val.uemail + '</td>';
-                      content += '</tr>';
-          });
-          $('#ex-table').append(content);
-      }
-  });
-}
-
-//CONTROLLER METHODS
 
 
-/*
-    Clean inputs
- */
- function _reset(){
-       $("#txt_productID").val("");
-       $("#txt_barCode").val("");
-       $("#txt_productName").val("");
-       $("#txt_productModel").val("");
-       $("#txt_price").val("");
-       $("#txt_oldPrice").val("");
-       $("#txt_trademark").val("");
-       $("#txt_productSize").val("");
-       $("#ta_descripPro").val("");
-       location.reload(); 
-  }
-
-/*
-    Listening page actions
- */function _onChangeStatusPage(catalog){
-       let materials = {};
-       let category,subCategory,id,barCode,name,model,lot,price,oldPrice,tradeMark,size,description;
-       $('#dd_category').change(()=>{category=$('#dd_category').val()}); 
-       $('#dd_subCategory').change(()=>{subCategory=$('#dd_subCategory').val()}); 
-       $("#txt_productID").keyup(()=>{id=$("#txt_productID").val()});
-       $("#txt_barCode").keyup(()=>{ barCode=$("#txt_barCode").val()});
-       $("#txt_productName").keyup(()=>{ name=$("#txt_productName").val()});
-       $("#txt_productModel").keyup(()=>{ model=$("#txt_productModel").val()});
-       $("#txt_lot").keyup(()=>{ lot=$("#txt_lot").val()});
-       $("#txt_price").keyup(()=>{ price=$("#txt_price").val()});
-       $("#txt_oldPrice").keyup(()=>{ oldPrice=$("#txt_oldPrice").val()});
-       $("#txt_trademark").keyup(()=>{ tradeMark=$("#txt_trademark").val()});
-       $("#txt_productSize").keyup(()=>{ size=$("#txt_productSize").val()});
-       $("#ta_descripPro").keyup(()=>{ description=$("#ta_descripPro").val()});
-       $('#dd_materials').change(function () {
-          $( "#dd_materials option:selected" ).each(function(i) {
-              materials[i]=$( this ).text();
-          });
-        })
-        .change();
-
-        $('#btn_goForm').click(()=>{showPage("frm_products",true)});
-        $('#btn_reset').click(()=>{_reset()});
-        $('#btn_push').click(()=> {_addProduct(category,subCategory,id,barCode,name,model,lot,price,oldPrice,tradeMark,size,description,materials,catalog)});
- 
-
-  }              
-/*
-    Controller data Product to be added
- */function _addProduct(category,subCategory,id,barCode,name,model,lot,price,oldPrice,tradeMark,size,description,materials,catalog){
-        const product={
-            category,
-            subCategory,
-            barCode,
-            name,
-            model,
-            lot,
-            price,
-            oldPrice,
-            tradeMark,
-            size,
-            description,
-            materials,
-            catalog
-        };
-       let route = CATEGORY_PRODUCTS+"/"+product.category+"/"+product.subCategory+"/"+id+"/";
-        console.log(route);
-        console.log(product);
-        addData(route,product)
-         
-    }
 
 
-    /*
-    
-   <tr class="gradeA">
-                                                <td>Misc</td>
-                                                <td>NetFront 3.1</td>
-                                                <td>Embedded devices</td>
-                                                <td class="center">-</td>
-                                                <td class="center">C</td>
-                                            </tr>
 
-    
-     <tr class="odd gradeX">
-                                                <td>Trident</td>
-                                                <td>Internet Explorer 4.0</td>
-                                                <td>Win 95+</td>
-                                                <td class="center">4</td>
-                                                <td class="center">X</td>
-                                            </tr>
-     */
+
 
 
 
